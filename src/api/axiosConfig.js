b8793cd9257336c6c5bb9d981b3e835be2cdb069
nasif952 <nasif.ahmed95@gmail.com>
2024-04-21 16:26:13 +0600

    change api url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://localhost:8080/',
+    baseURL:'cseprojectsem8-backend-production.up.railway.app/',
     headers: {"ngrok-skip-browser-warning": "true"}
 });
 
