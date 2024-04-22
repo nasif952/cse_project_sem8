e498c42b70c662bee47b7d21c9323b01a8a96fd2
nasif952 <nasif.ahmed95@gmail.com>
2024-04-22 23:06:07 +0600

    newss

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://localhost:8080/',
+    baseURL:'https://cseprojectsem8-backend-production.up.railway.app/',
     headers: {"ngrok-skip-browser-warning": "true"}
 });
 
